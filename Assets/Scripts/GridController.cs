316f99376aebdb10ff7c3bde828bf9dc4ca0a7fa
sheheryar-ahsan <57283023+sheheryar-ahsan@users.noreply.github.com>
2022-03-02 18:11:19 +0500

    Grid Mover V1.0
    Second Commit

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -2,24 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
 
 public class GridController : MonoBehaviour
 {
     public GameObject planePrefab;
+    public Canvas canvasPrefab;
     public TextMeshProUGUI birdText;
     public int numOfCols;
     public int numOfRows;
-    public float gapValue = 1.1f;
+    public float gapValue;
     public GameObject[,] planeArray;
+
     Dictionary<int, string> dicObject = new Dictionary<int, string>();
     void Start()
     {
+        AddToDictionary();
         GridSystem();
+        //RandomBirds();
     }
 
     void Update()
     {
-        
+
     }
     private void GridSystem()
     {
@@ -29,14 +34,30 @@ public class GridController : MonoBehaviour
         {
             for (int j = 0; j < numOfRows; j++)
             {
-                planeArray[i, j] = Instantiate(planePrefab, new Vector3(i*2.5f* gapValue,0.1f,j*2.5f* gapValue), planePrefab.transform.rotation);
-
+                planeArray[i, j] = Instantiate(planePrefab, new Vector3(i * 2.5f * gapValue, 0.1f, j * 2.5f * gapValue), planePrefab.transform.rotation);
+                TextMeshProUGUI textObj = Instantiate(birdText, new Vector3(i * 2.5f * gapValue, 0.15f, j * 2.5f * gapValue), birdText.transform.rotation);
+                textObj.transform.SetParent(canvasPrefab.transform);
+                birdText.text = RandomBirds();
             }
         }
     }
-    private void RandomBirds()
+    private void AddToDictionary()
     {
-        dicObject.Add(1, "Sparrow");
-        dicObject.Add(2, "Crow");
+        dicObject.Add(0, "Sparrow");
+        dicObject.Add(1, "Crow");
+        dicObject.Add(2, "Pigeon");
+        dicObject.Add(3, "Peacock");
+        dicObject.Add(4, "Parrot");
+        dicObject.Add(5, "Dove");
+        dicObject.Add(6, "Owl");
+        dicObject.Add(7, "Kingfisher");
+        dicObject.Add(8, "Kite");
+        dicObject.Add(9, "Eagle");
+        dicObject.Add(10, "Ostrich");
+    }
+    private string RandomBirds()
+    {
+        var randomKey = Random.Range(0, dicObject.Count);
+        return dicObject[randomKey].ToString();
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,17 +9,30 @@ public class PlayerController : MonoBehaviour
     private float desiredDuration = 3f;
     private float elapsedTime;
 
+    private bool isMoving;
+    public float speed;
+    private Vector3 oriPos;
+    private Vector3 targetPos;
+    private float timeToMove = 0.15f;
+
     private GridController gridController;
     public GameObject gridObject;
 
+    public int numOfCols;
+    public int numOfRows;
+    public float gapValue;
+
     void Start()
     {
-        gridController = gridController.gameObject.GetComponent<GridController>();
+        gridController = gridObject.gameObject.GetComponent<GridController>();
+        gapValue = gridController.gapValue;
+        numOfCols = gridController.numOfCols - 1;
+        numOfRows = gridController.numOfRows - 1;
     }
 
     void Update()
     {
-        
+        PlayerInput();
     }
     private void CorrectLerpMovement() // Correct way to use lerp
     {
@@ -28,11 +41,40 @@ public class PlayerController : MonoBehaviour
 
         transform.position = Vector3.Lerp(startPosition, endPosition, percentageCompleted);
     }
-    private void PlayerMovement()
+    private void PlayerInput()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.W) && !isMoving && (transform.position.z + 1 * gapValue * 2.5f <= numOfCols * gapValue * 2.5f))
         {
-
+            StartCoroutine(PlayerMovement((Vector3.forward) * gapValue * 2.5f));
         }
+        if (Input.GetKeyDown(KeyCode.S) && !isMoving && (transform.position.z - 1 * gapValue * 2.5f <= numOfCols * gapValue * 2.5f) && (transform.position.z - 1 >= 0)) // Special case for zero and greater
+        {
+            StartCoroutine(PlayerMovement((Vector3.back) * gapValue * 2.5f));
+        }
+        if (Input.GetKeyDown(KeyCode.A) && !isMoving && (transform.position.x - 1 * gapValue * 2.5f <= numOfRows * gapValue * 2.5f) && (transform.position.x - 1 >= 0)) // Special case for zero and greater
+        {
+            StartCoroutine(PlayerMovement((Vector3.left) * gapValue * 2.5f));
+        }
+        if (Input.GetKeyDown(KeyCode.D) && !isMoving && (transform.position.x + 1 * gapValue * 2.5f <= numOfRows * gapValue * 2.5f))
+        {
+            StartCoroutine(PlayerMovement((Vector3.right) * gapValue * 2.5f));
+        }
+    }
+    private IEnumerator PlayerMovement(Vector3 direction)
+    {
+        isMoving = true;
+        float elapsedTime = 0;
+
+        oriPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        targetPos = oriPos + direction;
+
+        while (elapsedTime < timeToMove)
+        {
+            elapsedTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(oriPos, targetPos, (elapsedTime / timeToMove));
+            yield return null;
+        }
+
+        isMoving = false;
     }
 }
